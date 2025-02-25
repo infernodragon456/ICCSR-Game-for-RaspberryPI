086ae54fd2a7c03a5ff5db0cd285a036ce3b2785
infernodragon456 <vighumandy420@gmail.com>
2025-02-25 22:58:42 +0530

    add continue button in choose_class

---
+++ scripts/continue.gd
@@ -0,0 +1,43 @@
+extends Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_5_pressed() -> void:
+	var continue_button = get_node("../Continue")  # Adjust the path if needed
+	continue_button.disabled = false
+	print(continue_button)
+	
+	
+
+
+func _on_button_4_pressed() -> void:
+	var continue_button = get_node("../Continue")  # Adjust the path if needed
+	continue_button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed() -> void:
+	var continue_button = get_node("../Continue")  # Adjust the path if needed
+	continue_button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed() -> void:
+	var continue_button = get_node("../Continue")  # Adjust the path if needed
+	continue_button.disabled = true
+	pass # Replace with function body.
+
+
+func _on_button_1_pressed() -> void:
+	var continue_button = get_node("../Continue")  # Adjust the path if needed
+	continue_button.disabled = true
+	pass # Replace with function body.
