062306e666b8b56f9248f819d5a82183dd950d1a
adityakedia006 <b22ee006@iitj.ac.in>
2025-04-30 02:40:33 +0530

    well done screen corrected

---
+++ scenes/welldone_new.gd
@@ -0,0 +1,21 @@
+extends Panel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (Global.finished_round):
+		visible = true
+		await get_tree().create_timer(2.0).timeout
+		
+		Global.reset_star_count()
+		Global.finished_round = false
+		get_tree().change_scene_to_file(Global.next_scene)
+		visible = false
+	else:
+		visible = false
+	pass
